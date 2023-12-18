872c6092e7a7e0092e55d9e365788fba29bdaf1a
nitish9966 <nitishbabubolla9966@gmail.com>
2023-12-18 21:35:59 +0530

    console log output changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function hello(){
-    console.log("Hello,This is my First node js application");
+    console.log("Hello,This is my First node js application This is created by Nitish");
 }
 
 hello();
